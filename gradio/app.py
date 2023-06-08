7a7f186b54ea8fd4f11db3d137081621e39fe355
ming-90 <minki319@naver.com>
2023-06-09 00:05:41 +0900

    Add tab for create embedding

--- gradio/app.py
+++ gradio/app.py
@@ -4,6 +4,9 @@
 import os
 import gradio as gr
 
+##################################
+# Functions
+##################################
 def get_coords(evt: gr.SelectData):
     print(f"x: {evt.index[1]} y: {evt.index[0]}")
     return evt.index[1], evt.index[0]
@@ -15,43 +18,57 @@ def image_click(
 ):
     print(coord_x, coord_y)
 
+def get_image_embedding():
+    print("ASD")
+
 ##################################
-# UI
+# Gradio UI
 ##################################
 
 with gr.Blocks() as app:
-    with gr.Row():
-        gr.Markdown("# Segment Anything Model")
+    with gr.Tab("SAM"):
+        with gr.Row():
+            gr.Markdown("# Segment Anything Model")
 
-    with gr.Row():
-        input_img = gr.Image(label="Input Image").style(height=500)
-        output_img = gr.Image(label="Output image").style(height=500)
+        with gr.Row():
+            input_img = gr.Image(label="Input Image").style(height=500)
+            output_img = gr.Image(label="Output image").style(height=500)
 
-    with gr.Row():
-        embedding_file = gr.File(type="file", label="SAM Embedding")
+        with gr.Row():
+            embedding_file = gr.File(type="file", label="SAM Embedding")
 
-    with gr.Row():
-        coord_x = gr.Number(label="Mouse coords x")
-        coord_y = gr.Number(label="Mouse coords y")
+        with gr.Row():
+            coord_x = gr.Number(label="Mouse coords x")
+            coord_y = gr.Number(label="Mouse coords y")
 
 
-    input_img.select(get_coords, None, [coord_x, coord_y]).then(
-        image_click,
-        [input_img, coord_x, coord_y],
-        output_img
-    )
-    gr.Examples(
-        examples=[
-            [
-                os.path.join(os.path.dirname(__file__), "examples/images/strawberry.png"),
-                os.path.join(os.path.dirname(__file__), "examples/embeddings/strawberry.npy"),
-                327,
-                467
-            ]
-        ],
-        inputs=[input_img, embedding_file, coord_x, coord_y],
-        outputs=output_img
-    )
+        gr.Examples(
+            examples=[
+                [
+                    os.path.join(os.path.dirname(__file__), "examples/images/strawberry.png"),
+                    os.path.join(os.path.dirname(__file__), "examples/embeddings/strawberry.npy"),
+                    327,
+                    467
+                ]
+            ],
+            inputs=[input_img, embedding_file, coord_x, coord_y],
+            outputs=output_img
+        )
+    with gr.Tab("Image Embedding"):
+
+        with gr.Row():
+            with gr.Column():
+                image = gr.Image(label="Input Image").style(height=500)
+            with gr.Column():
+                image_embedding_file = gr.File(label="image embedding file", type="file")
+                btn_embedding = gr.Button(value="Get image embedding")
+
+
+        btn_embedding.click(
+            get_image_embedding,
+            inputs=image,
+            outputs=image_embedding_file,
+        )
 
 
 
