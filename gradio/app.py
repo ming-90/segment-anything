ee3bb2cb6f91b6284b98854834c6059c3edb9ca2
ming-90 <minki319@naver.com>
2023-06-08 23:01:55 +0900

    Configurate UI strunt

--- gradio/app.py
+++ gradio/app.py
@@ -1,42 +1,56 @@
+"""
+
+"""
 import os
 import gradio as gr
 
-# Trasnforms
-patch_height_nums = 40
-patch_width_nums = 40
-patch_size = 14
-
-height, width = patch_size * patch_height_nums, patch_size * patch_width_nums
-
-def greet(name):
-    return "Hello " + name + "!"
-
-
-examples_sam_brush = [
-    [
-        os.path.join(os.path.dirname(__file__), "examples/strawberry.png"),
-        os.path.join(os.path.dirname(__file__), "examples/strawberry.png"),
-    ]
-]
 def get_coords(evt: gr.SelectData):
     print(f"x: {evt.index[1]} y: {evt.index[0]}")
     return evt.index[1], evt.index[0]
 
-def image_click(e):
-    print(e)
+def image_click(
+    input_img,
+    coord_x,
+    coord_y,
+):
+    print(coord_x, coord_y)
 
-with gr.Row():
-    gr.Markdown("# Inputs")
-with gr.Row():
-    input_img = gr.Image(label="Input Image")
+##################################
+# UI
+##################################
 
-input_img.select(get_coords, None).then(
-    fn = image_click,
-    input=[gr.SelectData]
-)
+with gr.Blocks() as app:
+    with gr.Row():
+        gr.Markdown("# Segment Anything Model")
+
+    with gr.Row():
+        input_img = gr.Image(label="Input Image").style(height=500)
+        output_img = gr.Image(label="Output image").style(height=500)
+
+    with gr.Row():
+        coord_x = gr.Number(label="Mouse coords x")
+        coord_y = gr.Number(label="Mouse coords y")
+
+
+    input_img.select(get_coords, None, [coord_x, coord_y]).then(
+        image_click,
+        [input_img, coord_x, coord_y],
+        output_img
+    )
+    gr.Examples(
+        examples=[
+            [
+                os.path.join(os.path.dirname(__file__), "examples/strawberry.png"),
+                327,
+                467
+            ]
+        ],
+        inputs=[input_img, coord_x, coord_y],
+        outputs=output_img
+    )
 
 
 
 
 if __name__ == "__main__":
-    demo.queue().launch(server_name="0.0.0.0")
+    app.queue().launch(server_name="0.0.0.0")
